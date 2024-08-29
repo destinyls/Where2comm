e580ab037659d378fef034b7a8186943725719eb
yanglei18 <yanglei20@mails.tsinghua.edu.cn>
2024-08-29 15:51:44 +0800

    m

--- train_script.sh
+++ train_script.sh
@@ -7,6 +7,6 @@ CUDA_VISIBLE_DEVICES=5 torchrun --nproc_per_node=1 opencood/tools/train.py --hyp
 python opencood/tools/inference.py --model_dir opencood/logs/dair_where2comm_max_multiscale_resnet_2024_08_16_08_25_05  --fusion_method  intermediate_with_comm
 
 
-python opencood/tools/inference.py --model_dir opencood/logs/mae_logs/dair_where2comm_max_multiscale_resnet_2024_08_24_09_15_24  --fusion_method  intermediate_with_comm
+python opencood/tools/inference.py --model_dir opencood/logs/mae_logs/dair_where2comm_max_multiscale_resnet_2024_08_26_22_17_52  --fusion_method  intermediate_with_comm
 
-CUDA_VISIBLE_DEVICES=3 python opencood/tools/train_single.py --hypes_yaml opencood/hypes_yaml/dair-v2x/dair_where2comm_max_multiscale_resnet.yaml --model_dir opencood/logs/mae_logs/dair_where2comm_max_multiscale_resnet_2024_08_26_22_17_52
+CUDA_VISIBLE_DEVICES=0 python opencood/tools/train_single.py --hypes_yaml opencood/hypes_yaml/dair-v2x/dair_where2comm_max_multiscale_resnet.yaml --model_dir opencood/logs/mae_logs/dair_where2comm_max_multiscale_resnet_2024_08_26_09_07_27
