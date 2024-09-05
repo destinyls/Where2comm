e068078fe5803e73be0817e352caa10d5b9d1bc6
yanglei18 <yanglei20@mails.tsinghua.edu.cn>
2024-09-05 23:35:21 +0800

    m

--- train_script.sh
+++ train_script.sh
@@ -12,4 +12,4 @@
 # CUDA_VISIBLE_DEVICES=4 python opencood/tools/train_single.py --hypes_yaml opencood/hypes_yaml/dair-v2x/dair_where2comm_max_multiscale_resnet_vehicle.yaml
 
 
-CUDA_VISIBLE_DEVICES=5 python opencood/tools/inference.py --model_dir opencood/logs/mae_logs/dair_where2comm_max_multiscale_resnet_2024_08_26_22_21_59  --fusion_method intermediate_with_comm
+CUDA_VISIBLE_DEVICES=6 python opencood/tools/inference.py --model_dir opencood/logs/no_mae_logs/dair_where2comm_max_multiscale_resnet_2024_09_04_23_11_03  --fusion_method intermediate_with_comm
