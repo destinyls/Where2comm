c4170735c7becb8ccdae523bb3bedc3f7ffc13ae
yanglei18 <yanglei20@mails.tsinghua.edu.cn>
2024-10-22 16:32:15 +0800

    m

--- opencood/tools/train_singleGPU.py
+++ opencood/tools/train_singleGPU.py
@@ -84,8 +84,8 @@ def main_worker(local_rank, nprocs, opt):
     else:
         if hypes['resume'] is not None:
             # model = train_utils.load_model_infra(hypes['resume'], model.module if distributed else model)   # 只加载infra端权重
-            model = train_utils.load_model_infra_veh_crhead(hypes['resume'], model.module if distributed else model)  # load infra veh head  for train_Flow_predict
-            # model = train_utils.load_whole_model(hypes['resume'], model.module if distributed else model)   # load whole for finetune_Head
+            # model = train_utils.load_model_infra_veh_crhead(hypes['resume'], model.module if distributed else model)  # load infra veh head  for train_Flow_predict
+            model = train_utils.load_whole_model(hypes['resume'], model.module if distributed else model)   # load whole for finetune_Head
         init_epoch = 0
         # if we train the model from scratch, we need to create a folder
         # to save the model,
